f2b21ac8ce31aaac98efa4cbc37f5a301899f822
wanggang424 <wanggang424@jd.com>
2022-11-17 13:59:14 +0800

    master

---
+++ src/main/java/com/example/jacocoinccovdemo/JacocoIncCovDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jacocoinccovdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacocoIncCovDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JacocoIncCovDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jacocoinccovdemo/JacocoIncCovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jacocoinccovdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacocoIncCovDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
